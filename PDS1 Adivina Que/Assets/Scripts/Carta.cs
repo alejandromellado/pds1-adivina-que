680a8aa547d78edfb47b0acee199889a1f9ab63e
Gilberto <gilber_8_1998@hotmail.com>
2021-02-12 16:37:59 -0700

    part1 lista
    shsh

---
+++ PDS1 Adivina Que/Assets/Scripts/Carta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    public Material colorCarta;
+    public int NumCarta = 0;
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+        print(NumCarta.ToString());
+    }
+
+    // Update is called once per frame
+    public void PonerColor(Material color_)
+    {
+        GetComponent<MeshRenderer>().material = color_;
+        colorCarta = color_;
+    }
+}
---
+++ PDS1 Adivina Que/Assets/Scripts/CrearCartas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearCartas : MonoBehaviour
+{
+    public GameObject CartaPrefab;
+    public int Ancho;
+    public int Alto;
+
+    public Material Blanco;
+    public Material Negro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Crear();
+    }
+
+
+    public void Crear()
+    {
+        int cont = 0;
+        for (int i = 0; i < Ancho; i++)
+        {
+            for (int x = 0; x < Alto; x++)
+            {
+                GameObject cartaTemp = Instantiate(CartaPrefab, new Vector3(x, 0, i), Quaternion.identity);
+                if ((i+x)% 2==0)
+                {
+                    cartaTemp.GetComponent<Carta>().PonerColor(Negro);
+                }
+                else
+                {
+                    cartaTemp.GetComponent<Carta>().PonerColor(Blanco);
+                }
+                cartaTemp.GetComponent<Carta>().NumCarta = cont;
+                cont++;
+            }
+        }
+    }
+
+    
+}

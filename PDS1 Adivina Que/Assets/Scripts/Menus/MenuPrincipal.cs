d0284a412c4705eff4771822ce58d724adabfbcd
Alejandro Mellado <alejandromellado7@gmail.com>
2021-02-13 00:14:35 -0700

    crear menu de niveles y de materias

---
+++ PDS1 Adivina Que/Assets/Scripts/Menus/MenuPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego.");
+        Application.Quit();
+    }
+}

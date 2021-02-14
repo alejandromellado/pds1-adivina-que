86facce48ee728418b815e44ee5c16e134e99304
Alejandro Mellado <alejandromellado7@gmail.com>
2021-02-13 23:56:58 -0700

    crear menu de galeria

--- PDS1 Adivina Que/Assets/Scripts/Menus/MenuPrincipal.cs
+++ PDS1 Adivina Que/Assets/Scripts/Menus/MenuPrincipal.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public void CargarJuego()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
     public void Salir()
     {
         Debug.Log("Saliendo del juego.");

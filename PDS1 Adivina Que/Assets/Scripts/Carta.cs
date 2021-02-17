d2d2a16c726a0f7d370d71526f95e679e4988b03
Gilberto <gilber_8_1998@hotmail.com>
2021-02-16 22:02:19 -0700

    Version 0.1
    jsjs

--- PDS1 Adivina Que/Assets/Scripts/Carta.cs
+++ PDS1 Adivina Que/Assets/Scripts/Carta.cs
@@ -5,17 +5,59 @@ using UnityEngine;
 public class Carta : MonoBehaviour
 {
     public Material colorCarta;
-    public int NumCarta = 0;
-    // Start is called before the first frame update
+    public int idCarta = 0;
+    public Vector3 posicionOriginal;
+    public Texture2D texturaAnverso;
+    public Texture2D texturaReverso;
+    public float tiempoDelay;
+    public GameObject crearCartas;
+    public bool Mostrando;
+
+    void Awake()
+    {
+        crearCartas = GameObject.Find("scripts");
+    }
+
+    void Start()
+    {
+        EsconderCarta();
+    }
+
     public void OnMouseDown()
     {
-        print(NumCarta.ToString());
+        print(idCarta.ToString());
+        MostrarCarta();
     }
 
     // Update is called once per frame
-    public void PonerColor(Material color_)
+    public void AsignarTextura(Texture2D _textura)
     {
-        GetComponent<MeshRenderer>().material = color_;
-        colorCarta = color_;
+       texturaAnverso= _textura;
     }
+
+    public void MostrarCarta()
+    {
+        if (!Mostrando && crearCartas.GetComponent<CrearCartas>().sePuedeMostrar)
+        {
+            Mostrando = true;
+            GetComponent<MeshRenderer>().material.mainTexture = texturaAnverso;
+            //Invoke("EsconderCarta", tiempoDelay);
+            crearCartas.GetComponent<CrearCartas>().HacerClick(this);
+        }
+        
+    }
+
+    public void EsconderCarta()
+    {
+        Invoke("Esconder", tiempoDelay);
+        crearCartas.GetComponent<CrearCartas>().sePuedeMostrar = false;
+    }
+
+    void Esconder()
+    {
+        GetComponent<MeshRenderer>().material.mainTexture = texturaReverso;
+        Mostrando = false;
+        crearCartas.GetComponent<CrearCartas>().sePuedeMostrar = true;
+    }
+
 }
--- PDS1 Adivina Que/Assets/Scripts/CrearCartas.cs
+++ PDS1 Adivina Que/Assets/Scripts/CrearCartas.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrearCartas : MonoBehaviour
 {
     public GameObject CartaPrefab;
-    public int Ancho;
-    public int Alto;
+    public int ancho;
+    //public int alto;
+    public Transform CartasParent;
+    private List<GameObject> cartas = new List<GameObject>();
 
-    public Material Blanco;
-    public Material Negro;
+    
+    public Texture2D[] texturas;
+
+    public int contadorClicks;
+    public Text textoContadorIntentos;
+    public  Carta CartaMostrada;
+    public bool sePuedeMostrar= true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,24 +29,96 @@ public class CrearCartas : MonoBehaviour
     public void Crear()
     {
         int cont = 0;
-        for (int i = 0; i < Ancho; i++)
+        for (int i = 0; i < ancho; i++)
         {
-            for (int x = 0; x < Alto; x++)
+            for (int x = 0; x < ancho; x++)
             {
-                GameObject cartaTemp = Instantiate(CartaPrefab, new Vector3(x, 0, i), Quaternion.identity);
-                if ((i+x)% 2==0)
-                {
-                    cartaTemp.GetComponent<Carta>().PonerColor(Negro);
-                }
-                else
-                {
-                    cartaTemp.GetComponent<Carta>().PonerColor(Blanco);
-                }
-                cartaTemp.GetComponent<Carta>().NumCarta = cont;
+                float factor = 9.0f/ancho;
+                Vector3 posicionTemp = new Vector3(x*factor, 0, i*factor);
+                GameObject cartaTemp = Instantiate(CartaPrefab,posicionTemp,Quaternion.Euler(new Vector3(0,180,0)));
+                cartaTemp.transform.localScale *= factor;
+                cartas.Add(cartaTemp);
+
+                cartaTemp.GetComponent<Carta>().posicionOriginal = posicionTemp;
+                cartaTemp.GetComponent<Carta>().idCarta = cont;
+                cartaTemp.transform.parent = CartasParent;
+
                 cont++;
             }
         }
+        AsignarTexturas();
+        Barajar();
     }
 
+    void AsignarTexturas()
+    {
+        for (int i = 0; i < cartas.Count; i++)
+        {
+            cartas[i].GetComponent<Carta>().AsignarTextura(texturas[(i)/2]);
+        }
+    }
+
+    void Barajar()
+    {
+        
+        int aleatorio;
+
+        for (int i = 0; i < cartas.Count; i++)
+        {
+            aleatorio = Random.Range(i, cartas.Count);
+
+            cartas[i].transform.position = cartas[aleatorio].transform.position;
+            cartas[aleatorio].transform.position = cartas[i].GetComponent<Carta>().posicionOriginal;
+
+            cartas[i].GetComponent<Carta>().posicionOriginal = cartas[i].transform.position;
+            cartas[aleatorio].GetComponent<Carta>().posicionOriginal = cartas[aleatorio].transform.position;
+        
+        }
+    }
+
+    public void HacerClick(Carta _carta)
+    {
+        if (CartaMostrada==null)
+        {
+            CartaMostrada = _carta;
+        }
+        else
+        {
+            
+            if (CompararCartas(_carta.gameObject, CartaMostrada.gameObject))
+            {
+                print("Felicidades!");
+            }
+            else
+            {
+                _carta.EsconderCarta();
+                CartaMostrada.EsconderCarta();
+                contadorClicks++;
+            }
+            CartaMostrada = null;
+        }
+        
+        ActualizarUI();
+    }
+
+    public bool CompararCartas(GameObject carta1, GameObject carta2)
+    {
+        bool resultado;
+        if (carta1.GetComponent<MeshRenderer>().material.mainTexture== carta2.GetComponent<MeshRenderer>().material.mainTexture)
+        {
+            resultado = true;
+        }
+        else
+        {
+            resultado = false;
+        }
+        return resultado;
+
+    }
+
+    public void ActualizarUI()
+    {
+        textoContadorIntentos.text = "Intentos: " + contadorClicks;
+    }
     
 }

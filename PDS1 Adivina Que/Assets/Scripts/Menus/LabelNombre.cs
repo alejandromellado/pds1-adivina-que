094ae31b8e5439f557f379a19a0560cde8bf06ae
Alejandro Mellado <alejandromellado7@gmail.com>
2021-02-23 15:19:29 -0700

    version 0.3.1
    crear menu de pausa en memorama

---
+++ PDS1 Adivina Que/Assets/Scripts/Menus/LabelNombre.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class LabelNombre : MonoBehaviour
+{
+    TextMeshProUGUI texto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        texto = GetComponent<TextMeshProUGUI>();
+
+        if (DataMantainer.Nombre != "")
+        {
+            texto.text = "Bienvenid@\n" + DataMantainer.Nombre;
+        }
+        else
+        {
+            texto.text = "Bienvenid@\n$NOMBRE";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- PDS1 Adivina Que/Assets/Scripts/Menus/MenuPrincipal.cs
+++ PDS1 Adivina Que/Assets/Scripts/Menus/MenuPrincipal.cs
@@ -8,25 +8,22 @@ using TMPro;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI labelNombre;
 
     void Start()
     {
-        if (DataMantainer.Nombre != null)
-        {
-            labelNombre.text = "Bienvenid@\n" + DataMantainer.Nombre;
-        }
-        else
-        {
-            labelNombre.text = "Bienvenid@\n$NOMBRE";
-        }
-            
+        
     }
 
     public void CargarJuego()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void CargarMenu()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
     }
 
     public void Salir()

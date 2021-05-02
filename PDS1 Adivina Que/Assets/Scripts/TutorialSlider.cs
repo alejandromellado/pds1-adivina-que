39e1c5d9032935c3c7c53bbc62abd20cb6b276f4
Alejandro Mellado <alejandromellado7@gmail.com>
2021-05-01 23:21:09 -0700

    terminar apartado de como jugar

--- PDS1 Adivina Que/Assets/Scripts/TutorialSlider.cs
+++ PDS1 Adivina Que/Assets/Scripts/TutorialSlider.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialSlider : MonoBehaviour
 {
-    [SerializeField] Sprite[] imagenes;
-    [SerializeField] string[] instrucciones;
+    [SerializeField] Sprite[] images;
+    [SerializeField] string[] instructions;
+    [SerializeField] TextMeshProUGUI label;
+    [SerializeField] Image image;
+
+    int currentSlide;
+    int totalSlides;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSlide = 0;
+        totalSlides = images.Length;
+
+        SetSlide(currentSlide);
     }
 
+    public void SetSlide(int number)
+    {
+        label.text = instructions[number];
+        image.sprite = images[number];
+    }
+
+    public void NextSlide()
+    {
+        if (currentSlide < totalSlides - 1)
+        {
+            currentSlide++;
+            SetSlide(currentSlide);
+        }
+    }
+
+    public void PreviousSlide()
+    {
+        if (currentSlide > 0)
+        {
+            currentSlide--;
+            SetSlide(currentSlide);
+        }
+    }
 }
